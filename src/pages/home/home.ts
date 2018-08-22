af5a2b660d06a563fd6f6f71eccaee55e8559f8e
Petunia Neta <Petunianeta@gmail.com>
2018-08-22 15:51:54 -0700

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Flashlight } from '@ionic-native/flashlight';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+available;
+  constructor(public navCtrl: NavController,private flashlight: Flashlight ) {
+    
   }
+  
+  On(){
+    this.flashlight.switchOn();
+  }
+  Off(){
+    this.flashlight.switchOff();
+
+  } 
 
 }
